7046472353848a22a6741d10cfa5b09f29caef80
ZainulabdeenOfficial <zu4425@gmail.com>
2025-07-08 23:04:22 +0500

    Add project files.

---
+++ API/Codepulse.API/Codepulse.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Codepulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Codepulse.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Catogrey> Categories { get; set; }
+    }
+}
---
+++ API/Codepulse.API/Codepulse.API/Models/Domain/Catogrey.cs
@@ -0,0 +1,13 @@
+﻿namespace Codepulse.API.Models.Domain
+{
+    public class Catogrey
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string UrlHandle { get; set; }
+
+
+    }
+}
