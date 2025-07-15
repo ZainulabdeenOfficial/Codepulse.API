f78cc69e934ba2c9b4cb6ebda0404487414bd9fc
ZainulabdeenOfficial <zu4425@gmail.com>
2025-07-15 22:47:42 +0500

    Blog List Component Added in UI

--- UI/codepulse/src/app/app.routes.ts
+++ UI/codepulse/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { CetagoreyListComponent } from './features/Cetagorey/cetagorey-list/cetagorey-list';
 import { AddCategorey } from './features/Cetagorey/add-categorey/add-categorey';
 import { EditCetagorey } from './features/Cetagorey/edit-cetagorey/edit-cetagorey';
+import { BlogpostList } from './features/Blog-Post/blogpost-list/blogpost-list';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/admin/category', pathMatch: 'full' },
   { path: 'admin/category', component: CetagoreyListComponent },
   { path: 'admin/category/add', component: AddCategorey },
   {path :'admin/category/:id', component: EditCetagorey},
+  {path:'admin/Blogpost',component:BlogpostList}
   
 ];
 
