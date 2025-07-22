069893255a50d09b01da89f910df26d291d20394
ZainulabdeenOfficial <zu4425@gmail.com>
2025-07-22 22:42:11 +0500

    add blog post model added

--- UI/codepulse/src/app/features/Blog-Post/add-blogpost/add-blogpost.ts
+++ UI/codepulse/src/app/features/Blog-Post/add-blogpost/add-blogpost.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { BlogPost } from '../models/add-blog-post.model';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-blogpost',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-blogpost.html',
   styleUrl: './add-blogpost.css'
 })
 export class AddBlogpost {
+
+  model : BlogPost;
+  
+  constructor (){
+    this.model = {
+      Title :'',
+      ShortDescription :'',
+      Content : '',
+      Author :'',
+      PublishedDate: new Date(),
+      IsVisible : true,
+      UrlHandle : '',
+      FeaturedImageUrl : ''
+    }
+  }
+
+  get publishedDateString(): string {
+    return this.model.PublishedDate ? this.model.PublishedDate.toISOString().split('T')[0] : '';
+  }
+
+  set publishedDateString(value: string) {
+    this.model.PublishedDate = new Date(value);
+  }
+
+  onSubmit(): void{
+    console.log('Form submitted', this.model);
+  }
 
 }
---
+++ UI/codepulse/src/app/features/Blog-Post/models/add-blog-post.model.ts
@@ -0,0 +1,11 @@
+export interface BlogPost{
+    Title: string,
+    ShortDescription: string,
+    Content: string,
+    FeaturedImageUrl: string,
+    UrlHandle: string,
+    Author: string,
+    PublishedDate: Date,
+    IsVisible: boolean
+
+}
