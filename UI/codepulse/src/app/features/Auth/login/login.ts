e4a75cc1f34a20a1aac49ea21e3cd4c6e48500d2
Zain Ul Abdeen <zu4425@gmail.com>
2025-09-22 22:27:35 +0500

    Cookies Added

--- UI/codepulse/src/app/features/Auth/login/login.ts
+++ UI/codepulse/src/app/features/Auth/login/login.ts
@@ -3,10 +3,11 @@ import { LoginRequest } from '../models/login-request.model';
 import { FormsModule } from '@angular/forms';
 import { Auth } from '../services/auth';
 import { response } from 'express';
-
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule,],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
@@ -14,7 +15,7 @@ export class Login {
 
   model : LoginRequest;
 
-  constructor(private authService:Auth){
+  constructor(private authService:Auth, private CookiesService:CookieService , private router:Router) {
     this.model = {
       email: '',
       password: ''
@@ -25,7 +26,13 @@ export class Login {
   onSubmit(){
     this.authService.login(this.model).subscribe({
       next:(response)=>{
-        console.log('Login successful',response);
+        // set auth cookies 
+
+        this.CookiesService.set('authorization',`Bearer ${response.token}`,undefined,'/',undefined,true,'Strict');
+
+        // Reddirect back to Home page
+        this.router.navigate(['/ ']);
+
       }
     })
    
